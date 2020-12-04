609406428c8e1e371b0fb475609000da71d7f11e
Sytronik Liu <sytronik210@gmail.com>
2020-12-05 01:17:59 +0900

    spec_to_image only supports f32

--- src/display.rs
+++ src/display.rs
@@ -1,9 +1,6 @@
 use image::{imageops, ImageBuffer, Luma};
 use image::{Rgb, RgbImage};
 use ndarray::prelude::*;
-use rustfft::num_traits::Float;
-use std::convert::TryInto;
-use std::ops::*;
 
 const COLORMAP: [[u8; 3]; 10] = [
     [0, 0, 4],
@@ -26,20 +23,16 @@ fn convert_grey_to_color(x: f32) -> Rgb<u8> {
         Rgb(COLORMAP[COLORMAP.len() - 1])
     } else {
         let ratio = position - index as f32;
-        let color_vec: Vec<u8> = COLORMAP[index]
-            .iter()
-            .zip(COLORMAP[index + 1].iter())
-            .map(|(&a, &b)| (ratio * b as f32 + (1. - ratio) * a as f32).round() as u8)
-            .collect();
-        Rgb(color_vec[..].try_into().unwrap())
+        let mut color = [0u8; 3];
+        for (i, c) in color.iter_mut().enumerate() {
+            *c = (ratio * COLORMAP[index + 1][i] as f32 + (1. - ratio) * COLORMAP[index][i] as f32)
+                .round() as u8;
+        }
+        Rgb(color)
     }
 }
 
-pub fn spec_to_image<A: Float + Sub + Div>(
-    spec_db: &Array2<A>,
-    nwidth: u32,
-    nheight: u32,
-) -> RgbImage {
+pub fn spec_to_image(spec_db: ArrayView2<f32>, nwidth: u32, nheight: u32) -> RgbImage {
     // let im = RgbImage::from_fn(
     //     spec_db.shape()[0] as u32, spec_db.shape()[1] as u32,
     //     |x, y| {
@@ -53,11 +46,10 @@ pub fn spec_to_image<A: Float + Sub + Div>(
         spec_db.shape()[0] as u32,
         spec_db.shape()[1] as u32,
         |x, y| {
-            Luma([(spec_db[[x as usize, spec_db.shape()[1] - y as usize - 1]]
-                .to_f32()
-                .unwrap()
-                - min_db)
-                / (max_db - min_db)])
+            Luma([
+                (spec_db[[x as usize, spec_db.shape()[1] - y as usize - 1]] - min_db)
+                    / (max_db - min_db),
+            ])
         },
     );
     let im = imageops::resize(&im, nwidth, nheight, imageops::FilterType::Lanczos3);
