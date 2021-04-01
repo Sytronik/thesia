7a8ba6cdffe73820131d50534895f254dac32409
Crackyachae <crackyachae@gmail.com>
2021-04-01 14:18:05 +0900

    [front] make filename truncated in when it is long

--- src/components/MainViewer/TrackInfo.js
+++ src/components/MainViewer/TrackInfo.js
@@ -5,6 +5,9 @@ function TrackInfo({trackid, trackinfo, height, selectTrack, showContextMenu}) {
   const track_info = useRef();
   const {filename, time, bit, sr} = trackinfo;
 
+  const paths = filename.split("/");
+  const name = paths.pop();
+
   useEffect(() => {
     if (track_info.current) {
       track_info.current.style.height = `${height}px`;
@@ -19,7 +22,10 @@ function TrackInfo({trackid, trackinfo, height, selectTrack, showContextMenu}) {
       onClick={selectTrack}
       onContextMenu={showContextMenu}
     >
-      <span className="filename">{filename}</span>
+      <span className="filename">
+        {paths.length ? <span className="paths">{paths.join("/")}</span> : null}
+        <span className={paths.length ? "name w-path" : "name"}>{name}</span>
+      </span>
       <span className="time">{time}</span>
       <span className="bit-sr">
         <span className="bit">{bit}</span> | <span className="sr">{sr}</span>
