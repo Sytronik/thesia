1bb5becb8f1dfe8e6b295b11676b52cfed4cf3ad
Crackyachae <crackyachae@gmail.com>
2022-12-12 13:38:05 +0900

    [front] separate track handle logic as useTracks
    (cherry picked from commit 6b20cd1e795789cc67c4267753261f76ba329876)

---
+++ src/renderer/hooks/useTracks.ts
@@ -0,0 +1,132 @@
+import {useRef, useState, useCallback} from "react";
+import {ipcRenderer} from "electron";
+import {SUPPORTED_TYPES} from "renderer/prototypes/constants";
+import NativeAPI from "../api";
+
+export default function useTracks() {
+  const [trackIds, setTrackIds] = useState<number[]>([]);
+  const [erroredList, setErroredList] = useState<number[]>([]);
+  const [refreshList, setRefreshList] = useState<IdChArr>([]);
+
+  const waitingIdsRef = useRef<number[]>([]);
+
+  const reloadTracks = useCallback(async (selectedIds: number[]) => {
+    try {
+      const reloadedIds = NativeAPI.reloadTracks(selectedIds);
+      const erroredIds = selectedIds.filter((id) => !reloadedIds.includes(id));
+      const needRefreshIds = await NativeAPI.applyTrackListChanges();
+
+      if (erroredIds && erroredIds.length) {
+        setErroredList(erroredIds);
+      }
+      if (needRefreshIds) {
+        setRefreshList(needRefreshIds);
+      }
+    } catch (err) {
+      console.log("Track reloads error", err);
+    }
+  }, []);
+
+  const addTracks = useCallback(
+    async (newPaths: string[], unsupportedPaths: string[]) => {
+      try {
+        const newIds: number[] = [];
+        const existingIds: number[] = [];
+        let invalidIds: number[] = [];
+        let invalidPaths: string[] = [];
+
+        newPaths.forEach((path, i, newPaths) => {
+          const id = NativeAPI.findIdByPath(path);
+          if (id !== -1) {
+            newPaths.splice(i, 1);
+            existingIds.push(id);
+          }
+        });
+
+        if (newPaths.length) {
+          for (let i = 0; i < newPaths.length; i += 1) {
+            if (waitingIdsRef.current.length) {
+              newIds.push(waitingIdsRef.current.shift() as number);
+            } else {
+              newIds.push(trackIds.length + i);
+            }
+          }
+
+          // nextSelectedIndexRef.current = trackIds.length;
+          const addedIds = NativeAPI.addTracks(newIds, newPaths);
+          setTrackIds((trackIds) => trackIds.concat(addedIds));
+
+          if (newIds.length !== addedIds.length) {
+            invalidIds = newIds.filter((id) => !addedIds.includes(id));
+            invalidPaths = invalidIds.map((id) => newPaths[newIds.indexOf(id)]);
+
+            waitingIdsRef.current = waitingIdsRef.current.concat(invalidIds);
+            if (waitingIdsRef.current.length > 1) {
+              waitingIdsRef.current.sort((a, b) => a - b);
+            }
+          }
+          if (unsupportedPaths.length || invalidPaths.length) {
+            ipcRenderer.send(
+              "show-file-open-err-msg",
+              unsupportedPaths,
+              invalidPaths,
+              SUPPORTED_TYPES,
+            );
+          }
+        }
+
+        if (existingIds.length) {
+          reloadTracks(existingIds);
+        } else {
+          const needRefreshIds = await NativeAPI.applyTrackListChanges();
+          if (needRefreshIds) {
+            setRefreshList(needRefreshIds);
+          }
+        }
+      } catch (err) {
+        console.log(err);
+        alert("File upload error");
+      }
+    },
+    [trackIds],
+  );
+
+  const ignoreError = (erroredId: number) => {
+    setErroredList(erroredList.filter((id) => ![erroredId].includes(id)));
+  };
+
+  const removeTracks = useCallback(
+    async (selectedIds) => {
+      try {
+        // nextSelectedIndexRef.current = trackIds.indexOf(selectedIds[0]);
+        NativeAPI.removeTracks(selectedIds);
+        setTrackIds((trackIds) => trackIds.filter((id) => !selectedIds.includes(id)));
+        setErroredList(erroredList.filter((id) => !selectedIds.includes(id)));
+
+        const needRefreshIds = await NativeAPI.applyTrackListChanges();
+        if (needRefreshIds) {
+          setRefreshList(needRefreshIds);
+        }
+
+        waitingIdsRef.current = waitingIdsRef.current.concat(selectedIds);
+        if (waitingIdsRef.current.length > 1) {
+          waitingIdsRef.current.sort((a, b) => a - b);
+        }
+      } catch (err) {
+        console.log(err);
+        alert("Could not remove track");
+      }
+    },
+    [trackIds, erroredList],
+  );
+
+  return {
+    trackIds,
+    erroredList,
+    refreshList,
+    reloadTracks,
+    addTracks,
+    removeTracks,
+    ignoreError,
+  };
+}
