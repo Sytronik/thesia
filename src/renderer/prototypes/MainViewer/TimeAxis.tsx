c1838c99c94f7085fc7c9b0139847517c89cf800
Crackyachae <crackyachae@gmail.com>
2023-01-08 12:36:56 +0900

    [front] design: cut off end of time ruler ...
    the portion of the time axis above the amp & frequency axis

--- src/renderer/prototypes/MainViewer/TimeAxis.tsx
+++ src/renderer/prototypes/MainViewer/TimeAxis.tsx
@@ -1,13 +1,20 @@
 import React, {forwardRef} from "react";
 import AxisCanvas from "renderer/modules/AxisCanvas";
-import {TIME_CANVAS_HEIGHT, TIME_MARKER_POS} from "../constants";
+import {
+  TIME_CANVAS_HEIGHT,
+  TIME_MARKER_POS,
+  AMP_CANVAS_WIDTH,
+  FREQ_CANVAS_WIDTH,
+} from "../constants";
+
+const AXIS_SPACE = AMP_CANVAS_WIDTH + FREQ_CANVAS_WIDTH;
 
 const TimeAxis = forwardRef((props: {width: number}, ref) => {
   const {width} = props;
   return (
     <AxisCanvas
       ref={ref}
-      width={width}
+      width={width - AXIS_SPACE}
       height={TIME_CANVAS_HEIGHT}
       markerPos={TIME_MARKER_POS}
       direction="H"
