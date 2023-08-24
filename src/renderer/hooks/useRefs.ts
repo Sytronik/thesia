f4b5557fb1c7dff3af01cd6edf7e226db548f919
Sytronik Liu <sytronik210@gmail.com>
2023-08-24 16:02:39 +0900

    prevent useRefs returns different register fn ...
    for every re-render

--- src/renderer/hooks/useRefs.ts
+++ src/renderer/hooks/useRefs.ts
@@ -1,21 +1,27 @@
 import React, {useRef, useCallback} from "react";
 
-type ReactRefsObject<T> = {
+type RefsObject<T> = {
   [key: string]: T;
 };
 
+type RegisterRefFnsObject<T> = {
+  [key: string]: (ref: T) => void;
+};
+
 function useRefs<T>(): [
-  React.MutableRefObject<ReactRefsObject<T>>,
+  React.MutableRefObject<RefsObject<T>>,
   (refName: string) => React.RefCallback<T>,
 ] {
-  const refs = useRef<ReactRefsObject<T>>({});
+  const refs = useRef<RefsObject<T>>({});
+  const registerRefFns = useRef<RegisterRefFnsObject<T>>({});
 
-  const register = useCallback(
-    (refName: string) => (ref: T) => {
-      refs.current[refName] = ref;
-    },
-    [],
-  );
+  const register = useCallback((refName: string) => {
+    if (!registerRefFns.current[refName])
+      registerRefFns.current[refName] = (ref: T) => {
+        refs.current[refName] = ref;
+      };
+    return registerRefFns.current[refName];
+  }, []);
 
   return [refs, register];
 }
