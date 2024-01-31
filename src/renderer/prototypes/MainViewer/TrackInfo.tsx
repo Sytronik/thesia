Sytronik/thesia

commit d39fb0df6f3fd6d57e0055f8a36896ba5c7d15ac

prevent masking ticklabels of both end of axis

src/renderer/prototypes/MainViewer/TrackInfo.tsx
@@ -3,7 +3,7 @@ import {showElectronContextMenu} from "renderer/lib/electron-sender";
 import TrackSummary from "./TrackSummary";
 import NativeAPI from "../../api";
 import styles from "./TrackInfo.scss";
-import {CHANNEL} from "../constants";
+import {CHANNEL, VERTICAL_AXIS_PADDING} from "../constants";
 
 type TrackInfoProps = {
   trackId: number;
@@ -46,7 +46,7 @@ function TrackInfo(props: TrackInfoProps) {
       <TrackSummary
         className={styles.TrackSummary}
         data={trackSummaryData}
-        height={(height + 2) * channelCount - 2}
+        height={(height + 2) * channelCount - 2 - VERTICAL_AXIS_PADDING * 2}
       />
       <div className={styles.channels}>{channels}</div>
     </div>

src/renderer/prototypes/constants.ts
@@ -157,3 +157,6 @@ export const DB_BOUNDARIES = createBoundaries(DB_TICK_NUM);
 export const MIN_TICK_SCALE_BOUNDARY = 80;
 export const MIN_HEIGHT = MIN_TICK_SCALE_BOUNDARY + 10;
 export const MAX_HEIGHT = 5000;
+
+export const VERTICAL_AXIS_PADDING = 5;
+export const HORIZONTAL_AXIS_PADDING = 0;

src/renderer/modules/AxisCanvas.tsx
@@ -14,13 +14,14 @@ type MarkerPosition = {
 type AxisCanvasProps = {
   width: number;
   height: number;
+  axisPadding: number;
   markerPos: MarkerPosition;
   direction: "H" | "V"; // stands for horizontal and vertical
   className: "timeRuler" | "ampAxis" | "freqAxis" | "dbAxis";
 };
 
 const AxisCanvas = forwardRef((props: AxisCanvasProps, ref) => {
-  const {width, height, markerPos, direction, className} = props;
+  const {width, height, axisPadding, markerPos, direction, className} = props;
   const axisCanvasElem = useRef<HTMLCanvasElement>(null);
   const axisCanvasCtxRef = useRef<CanvasRenderingContext2D>();
   const {MAJOR_TICK_POS, MINOR_TICK_POS, LABEL_POS, LABEL_LEFT_MARGIN} = markerPos;
@@ -57,12 +58,13 @@ const AxisCanvas = forwardRef((props: AxisCanvasProps, ref) => {
 
       if (direction === "H") {
         ctx.beginPath();
-        ctx.moveTo(0, height);
-        ctx.lineTo(width, height);
+        ctx.moveTo(axisPadding, height);
+        ctx.lineTo(width - axisPadding, height);
         ctx.stroke();
 
         markers.forEach((marker) => {
-          const [pxPosition, label] = marker;
+          const [axisPosition, label] = marker;
+          const pxPosition = axisPosition + axisPadding;
 
           ctx.beginPath();
           if (label) {
@@ -77,12 +79,13 @@ const AxisCanvas = forwardRef((props: AxisCanvasProps, ref) => {
         });
       } else {
         ctx.beginPath();
-        ctx.moveTo(0, 0);
-        ctx.lineTo(0, height);
+        ctx.moveTo(0, axisPadding);
+        ctx.lineTo(0, height - axisPadding);
         ctx.stroke();
 
         markers.forEach((marker) => {
-          const [pxPosition, label] = marker;
+          const [axisPosition, label] = marker;
+          const pxPosition = axisPosition + axisPadding;
 
           ctx.beginPath();
           if (label) {

src/renderer/modules/ImgCanvas.tsx
@@ -30,7 +30,13 @@ const ImgCanvas = forwardRef((props: ImgCanvasProps, ref) => {
 
   return (
     <>
-      <canvas className={styles.ImgCanvas} ref={canvasElem} height={height} width={width} />{" "}
+      <canvas
+        className={styles.ImgCanvas}
+        ref={canvasElem}
+        height={height}
+        width={width}
+        style={{width, height}}
+      />{" "}
       {/* TEMP */}
     </>
   );

src/renderer/prototypes/MainViewer/AmpAxis.tsx
@@ -1,6 +1,6 @@
 import React, {forwardRef} from "react";
 import AxisCanvas from "renderer/modules/AxisCanvas";
-import {AMP_CANVAS_WIDTH, AMP_MARKER_POS} from "../constants";
+import {AMP_CANVAS_WIDTH, AMP_MARKER_POS, VERTICAL_AXIS_PADDING} from "../constants";
 
 const AmpAxis = forwardRef((props: {height: number}, ref) => {
   const {height} = props;
@@ -9,6 +9,7 @@ const AmpAxis = forwardRef((props: {height: number}, ref) => {
       ref={ref}
       width={AMP_CANVAS_WIDTH}
       height={height}
+      axisPadding={VERTICAL_AXIS_PADDING}
       markerPos={AMP_MARKER_POS}
       direction="V"
       className="ampAxis"

src/renderer/prototypes/MainViewer/ColorMap.scss
@@ -1,16 +1,17 @@
-@import '../../styles/index.scss';
+@import "../../styles/index.scss";
 
 .colorMapHeader {
   @include body-label;
 
-  height: $time-ruler-height + $track-margin; // TEMP
+  height: $time-ruler-height; // + $track-margin; // TEMP
   text-align: center;
+  margin-left: 1.5em;
 }
 
 // color bar
 .colorBar {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
 
   flex: 0 0 auto;
   width: $colorbar-width;

src/renderer/prototypes/MainViewer/ColorMap.tsx
@@ -2,16 +2,23 @@ import React, {useRef, useLayoutEffect, useState, useCallback} from "react";
 import AxisCanvas from "renderer/modules/AxisCanvas";
 import ColorBarCanvas from "renderer/prototypes/MainViewer/ColorBarCanvas";
 import styles from "./ColorMap.scss";
-import {COLORBAR_CANVAS_WIDTH, DB_CANVAS_WIDTH, DB_MARKER_POS, MIN_HEIGHT} from "../constants";
+import {
+  COLORBAR_CANVAS_WIDTH,
+  DB_CANVAS_WIDTH,
+  DB_MARKER_POS,
+  MIN_HEIGHT,
+  VERTICAL_AXIS_PADDING,
+} from "../constants";
 
 type ColorMapProps = {
   height: number;
   setHeight: (height: number) => void;
+  colorBarHeight: number;
   dbAxisCanvasElem: React.RefObject<AxisCanvasHandleElement>;
 };
 
 function ColorMap(props: ColorMapProps) {
-  const {height, setHeight, dbAxisCanvasElem} = props;
+  const {height, setHeight, colorBarHeight, dbAxisCanvasElem} = props;
 
   const colorMapElem = useRef<HTMLDivElement>(null);
   const drawedColorBarElem = useCallback((ref: ColorBarCanvasHandleElement) => {
@@ -40,13 +47,18 @@ function ColorMap(props: ColorMapProps) {
 
   return (
     <div className={styles.colorBar} ref={colorMapElem}>
-      <div>
-        <div className={styles.colorMapHeader}>dB</div>
-        <ColorBarCanvas ref={drawedColorBarElem} width={COLORBAR_CANVAS_WIDTH} height={height} />
+      <div className={styles.colorMapHeader}>dB</div>
+      <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <ColorBarCanvas
+          ref={drawedColorBarElem}
+          width={COLORBAR_CANVAS_WIDTH}
+          height={colorBarHeight}
+        />
         <AxisCanvas
           ref={dbAxisCanvasElem}
           width={DB_CANVAS_WIDTH}
           height={height}
+          axisPadding={VERTICAL_AXIS_PADDING}
           markerPos={DB_MARKER_POS}
           direction="V"
           className="dbAxis"

src/renderer/prototypes/MainViewer/FreqAxis.tsx
@@ -1,6 +1,6 @@
 import React, {forwardRef} from "react";
 import AxisCanvas from "renderer/modules/AxisCanvas";
-import {FREQ_CANVAS_WIDTH, FREQ_MARKER_POS} from "../constants";
+import {FREQ_CANVAS_WIDTH, FREQ_MARKER_POS, VERTICAL_AXIS_PADDING} from "../constants";
 
 const FreqAxis = forwardRef((props: {height: number}, ref) => {
   const {height} = props;
@@ -9,6 +9,7 @@ const FreqAxis = forwardRef((props: {height: number}, ref) => {
       ref={ref}
       width={FREQ_CANVAS_WIDTH}
       height={height}
+      axisPadding={VERTICAL_AXIS_PADDING}
       markerPos={FREQ_MARKER_POS}
       direction="V"
       className="freqAxis"

commit 53633fc1e0c80afc7f799002fb9719d782e646e3

fix layout that were broken by d39fb0df
change vertical axis padding to be consistent with layout unit

src/renderer/prototypes/MainViewer/MainViewer.module.scss
@@ -1,4 +1,4 @@
-@import '../../styles/index.scss';
+@import "../../styles/index.scss";
 
 .MainViewer {
   position: relative;
@@ -56,7 +56,6 @@
 .chCanvases {
   display: flex;
   align-items: center;
-  margin-bottom: $track-margin;
 
   canvas {
     display: block;

src/renderer/prototypes/MainViewer/TrackInfo.module.scss
@@ -4,7 +4,6 @@
   display: flex;
   flex: 0 0 auto;
   position: relative;
-  margin-bottom: $track-margin;
 }
 
 // track summary
@@ -48,9 +47,5 @@
 
   > span {
     @include body-assist;
-  }
-
-  &:not(:last-child) {
-    margin-bottom: $track-margin;
   }
 }

src/renderer/prototypes/MainViewer/TrackInfo.tsx
@@ -54,8 +54,8 @@ const TrackInfo = forwardRef((props: TrackInfoProps, ref) => {
       onClick={(e) => selectTrack(e, trackId)} // TODO: need optimization?
       onContextMenu={(e) => showTrackContextMenu(e, trackId)} // TODO: need optimization?
       style={{
-        padding: `${VERTICAL_AXIS_PADDING}px 0`,
-        height: channelHeight * trackIdCh.length + 2 * (trackIdCh.length - 1),
+        margin: `${VERTICAL_AXIS_PADDING}px 0`,
+        height: channelHeight * trackIdCh.length - 2 * VERTICAL_AXIS_PADDING,
       }}
     >
       <MemoizedTrackSummary className={styles.TrackSummary} data={trackSummary} />

src/renderer/prototypes/constants.ts
@@ -184,7 +184,7 @@ export const MIN_TICK_SCALE_BOUNDARY = 80;
 export const MIN_HEIGHT = MIN_TICK_SCALE_BOUNDARY + 70;
 export const MAX_HEIGHT = 5000;
 
-export const VERTICAL_AXIS_PADDING = 5;
+export const VERTICAL_AXIS_PADDING = 4;
 export const HORIZONTAL_AXIS_PADDING = 0;
 
 export const MAX_PX_PER_SEC = 384000;