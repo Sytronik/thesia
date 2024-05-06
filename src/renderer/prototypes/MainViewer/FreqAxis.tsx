ce208ba9a33574ddd990defd6e381b9a10c012e8
Sytronik Liu <sytronik210@gmail.com>
2024-05-06 15:21:21 +0900

    fix height of FreqAxis to be set properly

--- src/renderer/prototypes/MainViewer/FreqAxis.tsx
+++ src/renderer/prototypes/MainViewer/FreqAxis.tsx
@@ -1,13 +1,14 @@
 import React, {forwardRef} from "react";
+import useStore from "renderer/hooks/useStore";
 import AxisCanvas from "renderer/modules/AxisCanvas";
 import {FREQ_CANVAS_WIDTH, FREQ_MARKER_POS, VERTICAL_AXIS_PADDING} from "../constants/tracks";
 
-const FreqAxis = forwardRef(({height}: {height: number}, ref) => {
+const FreqAxis = forwardRef((_, ref) => {
   return (
     <AxisCanvas
       ref={ref}
       width={FREQ_CANVAS_WIDTH}
-      height={height}
+      height={useStore().getHeight()}
       axisPadding={VERTICAL_AXIS_PADDING}
       markerPos={FREQ_MARKER_POS}
       direction="V"
