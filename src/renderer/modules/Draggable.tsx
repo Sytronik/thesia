Sytronik/thesia

commit 0f91bc99008836fda7ca9f77a387c42c9f193c15

fix: double clicking axis was sometimes ignored

src/renderer/modules/Draggable.tsx
@@ -96,7 +96,6 @@ function Draggable<T extends string, U>(props: DraggingProps<T, U>) {
       calcCursorPosFunc(e, rect),
       rect,
     );
-    onDragging(e);
 
     const bodyElem = document.querySelector("body");
     if (bodyElem) {

src/renderer/prototypes/MainViewer/AmpAxis.tsx
@@ -101,7 +101,7 @@ const AmpAxis = forwardRef((props: AmpAxisProps, ref) => {
     e.preventDefault();
     if (e.button === 0 && e.detail === 2) {
       e.stopPropagation();
-      setAmpRange([...DEFAULT_AMP_RANGE]);
+      setTimeout(() => setAmpRange([...DEFAULT_AMP_RANGE]));
     }
   });
 

commit c2d1c3744387006649614ecc5adeb9559973da29

fix: clicking overview doesn't work (caused by 0f91bc9)

src/renderer/modules/Draggable.tsx
@@ -106,6 +106,7 @@ function Draggable<T extends string, U>(props: DraggingProps<T, U>) {
       });
     }
     draggedRef.current = false;
+    onDragging(e);
     document.addEventListener("mousemove", onDragging);
     document.addEventListener("mouseup", onMouseUp, {once: true});
   };