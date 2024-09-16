0a647cbd55a83881956ff7f3a03fba5f8de7c513
Sytronik Liu <sytronik210@gmail.com>
2024-09-16 11:15:51 +0900

    bump react, throttle-debounce, napi-cli versions

--- release/backend/index.js
+++ release/backend/index.js
@@ -224,17 +224,32 @@ switch (platform) {
         }
         break
       case 'arm':
-        localFileExisted = existsSync(
-          join(__dirname, 'thesia-backend.linux-arm-gnueabihf.node')
-        )
-        try {
-          if (localFileExisted) {
-            nativeBinding = require('./thesia-backend.linux-arm-gnueabihf.node')
-          } else {
-            nativeBinding = require('thesia-backend-linux-arm-gnueabihf')
+        if (isMusl()) {
+          localFileExisted = existsSync(
+            join(__dirname, 'thesia-backend.linux-arm-musleabihf.node')
+          )
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./thesia-backend.linux-arm-musleabihf.node')
+            } else {
+              nativeBinding = require('thesia-backend-linux-arm-musleabihf')
+            }
+          } catch (e) {
+            loadError = e
           }
-        } catch (e) {
-          loadError = e
+        } else {
+          localFileExisted = existsSync(
+            join(__dirname, 'thesia-backend.linux-arm-gnueabihf.node')
+          )
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./thesia-backend.linux-arm-gnueabihf.node')
+            } else {
+              nativeBinding = require('thesia-backend-linux-arm-gnueabihf')
+            }
+          } catch (e) {
+            loadError = e
+          }
         }
         break
       case 'riscv64':
