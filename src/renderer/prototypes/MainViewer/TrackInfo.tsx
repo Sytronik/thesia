983f4929fc825d4ddcfcb9849f25abf27e7ea0ac
Sytronik Liu <sytronik210@gmail.com>
2024-09-21 21:37:13 +0900

    set deps args of useDrag & useDrop

--- src/renderer/prototypes/MainViewer/TrackInfo.tsx
+++ src/renderer/prototypes/MainViewer/TrackInfo.tsx
@@ -54,71 +54,77 @@ const TrackInfo = forwardRef((props: TrackInfoProps, ref) => {
   });
   useImperativeHandle(ref, () => imperativeHandleRef.current, []);
 
-  const [{handlerId}, drop] = useDrop<DragItem, void, {handlerId: Identifier | null}>({
-    accept: ItemTypes.TRACK,
-    collect(monitor) {
-      return {
-        handlerId: monitor.getHandlerId(),
-      };
+  const [{handlerId}, drop] = useDrop<DragItem, void, {handlerId: Identifier | null}>(
+    {
+      accept: ItemTypes.TRACK,
+      collect(monitor) {
+        return {
+          handlerId: monitor.getHandlerId(),
+        };
+      },
+      hover(item: DragItem, monitor) {
+        if (!trackInfoElem.current) {
+          return;
+        }
+        const dragIndex = item.index;
+        const hoverIndex = index;
+
+        // Don't replace items with themselves
+        if (dragIndex === hoverIndex) {
+          return;
+        }
+
+        // Determine rectangle on screen
+        const hoverBoundingRect = trackInfoElem.current?.getBoundingClientRect();
+
+        // Get vertical middle
+        const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+        // Determine mouse position
+        const clientOffset = monitor.getClientOffset();
+
+        // Get pixels to the top
+        const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
+
+        // Only perform the move when the mouse has crossed half of the items height
+        // When dragging downwards, only move when the cursor is below 50%
+        // When dragging upwards, only move when the cursor is above 50%
+
+        // Dragging downwards
+        if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+          return;
+        }
+
+        // Dragging upwards
+        if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+          return;
+        }
+
+        // Time to actually perform the action
+        onDnd(dragIndex, hoverIndex);
+
+        // Note: we're mutating the monitor item here!
+        // Generally it's better to avoid mutations,
+        // but it's good here for the sake of performance
+        // to avoid expensive index searches.
+        item.index = hoverIndex;
+      },
     },
-    hover(item: DragItem, monitor) {
-      if (!trackInfoElem.current) {
-        return;
-      }
-      const dragIndex = item.index;
-      const hoverIndex = index;
+    [index, onDnd],
+  );
 
-      // Don't replace items with themselves
-      if (dragIndex === hoverIndex) {
-        return;
-      }
-
-      // Determine rectangle on screen
-      const hoverBoundingRect = trackInfoElem.current?.getBoundingClientRect();
-
-      // Get vertical middle
-      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-
-      // Determine mouse position
-      const clientOffset = monitor.getClientOffset();
-
-      // Get pixels to the top
-      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
-
-      // Only perform the move when the mouse has crossed half of the items height
-      // When dragging downwards, only move when the cursor is below 50%
-      // When dragging upwards, only move when the cursor is above 50%
-
-      // Dragging downwards
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return;
-      }
-
-      // Dragging upwards
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-        return;
-      }
-
-      // Time to actually perform the action
-      onDnd(dragIndex, hoverIndex);
-
-      // Note: we're mutating the monitor item here!
-      // Generally it's better to avoid mutations,
-      // but it's good here for the sake of performance
-      // to avoid expensive index searches.
-      item.index = hoverIndex;
+  const [{isDragging}, drag] = useDrag(
+    {
+      type: ItemTypes.TRACK,
+      item: () => {
+        return {id, index};
+      },
+      collect: (monitor: any) => ({
+        isDragging: monitor.isDragging(),
+      }),
     },
-  });
-
-  const [{isDragging}, drag] = useDrag({
-    type: ItemTypes.TRACK,
-    item: () => {
-      return {id, index};
-    },
-    collect: (monitor: any) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  });
+    [id, index],
+  );
 
   const opacity = isDragging ? 0 : 1;
   drag(drop(trackInfoElem));
