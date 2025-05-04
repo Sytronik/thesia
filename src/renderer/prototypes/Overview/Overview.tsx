b6be5f6fbf5094a08436953d44c57e96f5404c70
Sytronik <sytronik210@gmail.com>
2025-05-05 01:02:07 +0900

    let React update Overview

--- src/renderer/prototypes/Overview/Overview.tsx
+++ src/renderer/prototypes/Overview/Overview.tsx
@@ -1,12 +1,4 @@
-import React, {
-  useRef,
-  useEffect,
-  useMemo,
-  forwardRef,
-  useImperativeHandle,
-  useContext,
-  useCallback,
-} from "react";
+import React, {useRef, useEffect, useMemo, useContext, useCallback} from "react";
 import useEvent from "react-use-event-hook";
 import {DevicePixelRatioContext} from "renderer/contexts";
 import styles from "./Overview.module.scss";
@@ -22,18 +14,25 @@ const THICKNESS = 3;
 type OverviewProps = {
   selectedTrackId: number | null;
   maxTrackSec: number;
+  startSec: number;
+  lensDurationSec: number;
   moveLens: (sec: number, anchorRatio: number) => void;
   resizeLensLeft: (sec: number) => void;
   resizeLensRight: (sec: number) => void;
 };
 
-type ArgsGetOverview = [trackId: number, width: number, height: number];
-type ArgsLens = [durationSec: number, startSec: number, lensDurationSec: number];
-
 type OverviewCursorState = "left" | "right" | "inlens" | "outlens";
 
-const Overview = forwardRef((props: OverviewProps, ref) => {
-  const {selectedTrackId, maxTrackSec, moveLens, resizeLensLeft, resizeLensRight} = props;
+function Overview(props: OverviewProps) {
+  const {
+    selectedTrackId,
+    maxTrackSec,
+    startSec,
+    lensDurationSec,
+    moveLens,
+    resizeLensLeft,
+    resizeLensRight,
+  } = props;
   const devicePixelRatio = useContext(DevicePixelRatioContext);
   const durationSec = useMemo(
     () => (selectedTrackId !== null ? BackendAPI.getLengthSec(selectedTrackId) : 0),
@@ -45,23 +44,21 @@ const Overview = forwardRef((props: OverviewProps, ref) => {
 
   const resizeObserverRef = useRef<ResizeObserver | null>(null);
   const lensCtxRef = useRef<CanvasRenderingContext2D | null>(null);
-  const prevArgsRef = useRef<ArgsGetOverview | null>(null);
-  const prevArgsLensRef = useRef<ArgsLens | null>(null);
 
-  const calcPxPerSec = useEvent(() => {
+  const calcPxPerSec = useCallback(() => {
     const width = lensElem.current?.clientWidth ?? 0;
     return width / maxTrackSec;
-  });
+  }, [maxTrackSec]);
 
-  const drawLens = (trackDurationSec: number, startSec: number, lensDurationSec: number) => {
+  const drawLens = useCallback(() => {
     const ctx = lensCtxRef.current;
     if (!lensElem.current || !ctx) return;
     const {clientWidth: width, clientHeight: height} = lensElem.current;
     const pxPerSec = calcPxPerSec();
     const lensEndSec = (startSec + lensDurationSec) * pxPerSec;
-    const endSec = trackDurationSec * pxPerSec;
+    const endSec = durationSec * pxPerSec;
     ctx.clearRect(0, 0, width, height);
-    if (trackDurationSec < maxTrackSec) {
+    if (durationSec < maxTrackSec) {
       ctx.save();
       ctx.fillStyle = OUT_TRACK_FILL_STYLE;
       ctx.fillRect(endSec, 0, width, height);
@@ -78,90 +75,81 @@ const Overview = forwardRef((props: OverviewProps, ref) => {
     );
     ctx.stroke();
     if (width > lensEndSec) ctx.fillRect(lensEndSec, 0, width - lensEndSec, height);
-    prevArgsLensRef.current = [trackDurationSec, startSec, lensDurationSec];
-  };
+  }, [calcPxPerSec, durationSec, lensDurationSec, maxTrackSec, startSec]);
 
-  const draw = useEvent(async (startSec: number, lensDurationSec: number, forced = false) => {
+  const drawLensRef = useRef(drawLens);
+  useEffect(() => {
+    drawLensRef.current = drawLens;
+    // Request a redraw only when the draw function or its dependencies change
+    const animationFrameId = requestAnimationFrame(() => {
+      // Ensure drawRef.current exists and call it
+      if (drawLensRef.current) drawLensRef.current();
+    });
+
+    // Cleanup function to cancel the frame if the component unmounts
+    // or if dependencies change again before the frame executes
+    return () => cancelAnimationFrame(animationFrameId);
+  }, [drawLens]);
+
+  const draw = useCallback(async () => {
     if (!backgroundElem.current || !lensElem.current) return;
 
     const backgroundCtx = backgroundElem.current.getContext("bitmaprenderer");
 
     if (selectedTrackId === null) {
-      if (forced || prevArgsRef.current !== null) {
-        backgroundCtx?.transferFromImageBitmap(null);
-        lensCtxRef.current?.clearRect(
-          0,
-          0,
-          lensElem.current.clientWidth,
-          lensElem.current.clientHeight,
-        );
-      }
-      prevArgsRef.current = null;
-      prevArgsLensRef.current = null;
+      backgroundCtx?.transferFromImageBitmap(null);
+      lensCtxRef.current?.clearRect(
+        0,
+        0,
+        lensElem.current.clientWidth,
+        lensElem.current.clientHeight,
+      );
       return;
-    }
-
-    const argsLens: ArgsLens = [durationSec, startSec, lensDurationSec];
-    if (
-      forced ||
-      prevArgsLensRef.current === null ||
-      prevArgsLensRef.current.some((v, i) => Math.abs(argsLens[i] - v) > 1e-3)
-    ) {
-      drawLens(durationSec, startSec, lensDurationSec);
     }
 
     if (!backgroundCtx) return;
     const rect = backgroundElem.current.getBoundingClientRect();
-    const args: ArgsGetOverview = [selectedTrackId, rect.width, rect.height];
-    if (
-      forced ||
-      prevArgsRef.current === null ||
-      prevArgsRef.current.some((v, i) => Math.abs(args[i] - v) > 1e-3)
-    ) {
-      let imbmp = null;
-      if (rect.width >= 1) {
-        const img = await BackendAPI.getOverview(
-          selectedTrackId,
-          rect.width,
-          rect.height,
-          devicePixelRatio,
-        );
-        if (img.buf.length > 0) {
-          const imdata = new ImageData(new Uint8ClampedArray(img.buf), img.width, img.height);
-          imbmp = await createImageBitmap(imdata);
-        }
-      }
-      backgroundCtx.transferFromImageBitmap(imbmp);
-      prevArgsRef.current = args;
+    if (rect.width < 1) {
+      backgroundCtx.transferFromImageBitmap(null);
+      return;
     }
+    const img = await BackendAPI.getOverview(
+      selectedTrackId,
+      rect.width,
+      rect.height,
+      devicePixelRatio,
+    );
+    if (img.buf.length > 0) {
+      const imdata = new ImageData(new Uint8ClampedArray(img.buf.buffer), img.width, img.height);
+      const imbmp = await createImageBitmap(imdata);
+      backgroundCtx.transferFromImageBitmap(imbmp);
+    }
+  }, [devicePixelRatio, selectedTrackId]);
+
+  useEffect(() => {
+    draw();
+  }, [draw]);
+
+  const resizeObserverCallback = useEvent(() => {
+    if (!lensElem.current) return;
+    lensElem.current.width = lensElem.current.clientWidth * devicePixelRatio;
+    lensElem.current.height = lensElem.current.clientHeight * devicePixelRatio;
+    const lensCtx = lensElem.current.getContext("2d", {desynchronized: true});
+    lensCtxRef.current = lensCtx;
+    if (!lensCtx) return;
+    lensCtx.scale(devicePixelRatio, devicePixelRatio);
+    lensCtx.lineWidth = LINE_WIDTH;
+    lensCtx.fillStyle = OUT_LENS_FILL_STYLE;
+    lensCtx.strokeStyle = LENS_STROKE_STYLE;
+    draw();
+    drawLens();
   });
 
   useEffect(() => {
     resizeObserverRef.current?.disconnect();
-    resizeObserverRef.current = new ResizeObserver(() => {
-      if (!lensElem.current) return;
-      lensElem.current.width = lensElem.current.clientWidth * devicePixelRatio;
-      lensElem.current.height = lensElem.current.clientHeight * devicePixelRatio;
-      const lensCtx = lensElem.current.getContext("2d", {desynchronized: true});
-      lensCtxRef.current = lensCtx;
-      if (!lensCtx) return;
-      lensCtx.scale(devicePixelRatio, devicePixelRatio);
-      lensCtx.lineWidth = LINE_WIDTH;
-      lensCtx.fillStyle = OUT_LENS_FILL_STYLE;
-      lensCtx.strokeStyle = LENS_STROKE_STYLE;
-      if (prevArgsLensRef.current)
-        draw(prevArgsLensRef.current[1], prevArgsLensRef.current[2], true);
-    });
+    resizeObserverRef.current = new ResizeObserver(resizeObserverCallback);
     if (lensElem.current) resizeObserverRef.current.observe(lensElem.current);
-  }, [draw, devicePixelRatio]);
-
-  useEffect(() => {
-    if (!prevArgsLensRef.current) return;
-    draw(prevArgsLensRef.current[1], prevArgsLensRef.current[2], true);
-  }, [maxTrackSec, draw]);
-
-  const imperativeInstanceRef = useRef<OverviewHandleElement>({draw});
-  useImperativeHandle(ref, () => imperativeInstanceRef.current, []);
+  }, [resizeObserverCallback]);
 
   const calcSecFromX = useEvent((cursorX: number, rect: DOMRect) => {
     const ratioX = cursorX / rect.width;
@@ -215,12 +203,9 @@ const Overview = forwardRef((props: OverviewProps, ref) => {
   );
 
   const determineCursorStates = useEvent((cursorX: number) => {
-    if (!prevArgsLensRef.current) return "outlens";
-
-    const [_trackDurationSec, startSec, lensDuratoinSec] = prevArgsLensRef.current;
     const pxPerSec = calcPxPerSec();
     const lensStartX = Math.round(startSec * pxPerSec);
-    const lensEndX = Math.round((startSec + lensDuratoinSec) * pxPerSec);
+    const lensEndX = Math.round((startSec + lensDurationSec) * pxPerSec);
     if (lensStartX - THICKNESS <= cursorX && cursorX <= lensStartX + THICKNESS) {
       return "left";
     }
@@ -235,9 +220,8 @@ const Overview = forwardRef((props: OverviewProps, ref) => {
 
   const calcDragAnchor = useEvent(
     (cursorState: OverviewCursorState, cursorPos: number, rect: DOMRect) => {
-      if (prevArgsLensRef.current !== null && cursorState === "inlens") {
+      if (cursorState === "inlens") {
         const sec = calcSecFromX(cursorPos, rect);
-        const [_trackDurationSec, startSec, lensDurationSec] = prevArgsLensRef.current;
         return (sec - startSec) / lensDurationSec;
       }
       return 0.5;
@@ -262,7 +246,6 @@ const Overview = forwardRef((props: OverviewProps, ref) => {
       </Draggable>
     </div>
   );
-});
-Overview.displayName = "Overview";
+}
 
 export default React.memo(Overview);
