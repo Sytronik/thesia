a10e2dcd2ad6f00c467ccddddc68ee24204212eb
Sytronik <sytronik210@gmail.com>
2025-05-12 01:37:49 +0900

    parallelize calculation of spec mipmap

--- src_backend/backend/visualize/mipmap.rs
+++ src_backend/backend/visualize/mipmap.rs
@@ -1,5 +1,6 @@
 use fast_image_resize::pixels;
 use ndarray::prelude::*;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 use super::super::spectrogram::SpecSetting;
 
@@ -14,42 +15,50 @@ pub struct Mipmaps {
 impl Mipmaps {
     pub fn new(spec_img: Array2<pixels::F32>, max_size: u32) -> Self {
         let (orig_height, orig_width) = (spec_img.shape()[0], spec_img.shape()[1]);
-        let mut mipmaps = vec![vec![spec_img]];
+        let mut sizes = vec![vec![(orig_width as u32, orig_height as u32)]];
         let mut skip = true; // skip the first (original) mipmap
         let mut height = orig_height as f64;
         loop {
-            let mut width = orig_width as f64;
             if !skip {
-                mipmaps.push(vec![]);
+                sizes.push(vec![]);
             }
+            let height_u32 = height.round() as u32;
+            let mut width = orig_width as f64;
             loop {
+                let width_u32 = width.round() as u32;
                 if skip {
                     skip = false;
                 } else {
-                    let resized = resize(
-                        mipmaps[0][0].view(),
-                        width.round() as u32,
-                        height.round() as u32,
-                    );
-                    let i = mipmaps.len() - 1;
-                    mipmaps[i].push(resized);
+                    let i = sizes.len() - 1;
+                    sizes[i].push((width_u32, height_u32));
                 }
-                if (width.round() as u32) == max_size {
+                if (width_u32) == max_size {
                     break;
                 }
                 width /= 2.;
-                if (width.round() as u32) < max_size {
+                if (width_u32) < max_size {
                     width = max_size as f64;
                 }
             }
-            if (height.round() as u32) == max_size {
+            if (height_u32) == max_size {
                 break;
             }
             height /= 2.;
-            if (height.round() as u32) < max_size {
+            if (height_u32) < max_size {
                 height = max_size as f64;
             }
         }
+
+        let mut mipmaps: Vec<Vec<_>> = sizes
+            .into_par_iter()
+            .map(|_sizes| {
+                _sizes
+                    .into_par_iter()
+                    .map(|(width, height)| resize(spec_img.view(), width, height))
+                    .collect()
+            })
+            .collect();
+        mipmaps[0].insert(0, spec_img);
         Self { mipmaps, max_size }
     }
 
