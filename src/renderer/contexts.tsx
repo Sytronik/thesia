fc2fcc86d86a88c7b495e51e760961cd96ffc12b
Sytronik <sytronik210@gmail.com>
2025-05-28 18:08:49 +0900

    use dpr without round

--- src/renderer/contexts.tsx
+++ src/renderer/contexts.tsx
@@ -4,7 +4,7 @@ import {useDevicePixelRatio} from "use-device-pixel-ratio";
 export const DevicePixelRatioContext = createContext(1);
 
 export function DevicePixelRatioProvider({children}: {children: React.ReactElement}) {
-  const dpr = useDevicePixelRatio(); // TODO: no round?
+  const dpr = useDevicePixelRatio({round: false});
   return (
     <DevicePixelRatioContext.Provider value={dpr}>{children}</DevicePixelRatioContext.Provider>
   );
