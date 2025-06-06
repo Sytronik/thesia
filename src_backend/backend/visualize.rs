e932fef6f9721a6ff1b7bb06f9c107c6dcfd4211
Sytronik <sytronik210@gmail.com>
2025-06-06 19:10:44 +0900

    drawing_wav mod -> drawing mod

--- src_backend/backend/visualize.rs
+++ src_backend/backend/visualize.rs
@@ -1,5 +1,5 @@
 mod axis;
-mod drawing_wav;
+mod drawing;
 mod mipmap;
 mod resample;
 mod slice_args;
@@ -8,7 +8,7 @@ pub use axis::{
     calc_amp_axis_markers, calc_dB_axis_markers, calc_freq_axis_markers, calc_time_axis_markers,
     convert_freq_label_to_hz, convert_hz_to_label, convert_sec_to_label, convert_time_label_to_sec,
 };
-pub use drawing_wav::{
+pub use drawing::{
     OverviewDrawingInfoInternal, WavDrawingInfoCache, WavDrawingInfoInternal, WavDrawingInfoKind,
     convert_spectrogram_to_img,
 };
