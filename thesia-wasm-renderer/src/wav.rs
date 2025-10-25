9f9278e8aae7587b703c690c15cefed27b44b2bf
Sytronik <sytronik210@gmail.com>
2025-10-25 21:43:50 +0900

    refactor wasm-renderer/wav.rs ...
    - reorder functions / structs as pub -> pub(crate) -> private
    - transform_line_envelopes -> WavCache::transform_line_envelopes

--- thesia-wasm-renderer/src/wav.rs
+++ thesia-wasm-renderer/src/wav.rs
@@ -12,6 +12,7 @@ use crate::simd::{add_scalar_inplace, clamp_inplace, find_min_max, fused_mul_add
 pub(crate) const WAV_COLOR: &str = "rgb(19, 137, 235)";
 pub(crate) const WAV_CLIPPING_COLOR: &str = "rgb(196, 34, 50)";
 const WAV_BORDER_COLOR: &str = "rgb(0, 0, 0)";
+
 const WAV_IMG_SCALE: f32 = 2.0;
 const WAV_BORDER_WIDTH: f32 = 1.5;
 const WAV_LINE_WIDTH: f32 = 1.75;
@@ -23,6 +24,184 @@ const CACHE_HEIGHT: f32 = 10000.0;
 pub(crate) static WAV_CACHES: LazyLock<RwLock<HashMap<String, WavCache>>> =
     LazyLock::new(|| RwLock::new(HashMap::new()));
 pub(crate) static DEVICE_PIXEL_RATIO: LazyLock<AtomicF32> = LazyLock::new(|| AtomicF32::new(1.0));
+
+#[wasm_bindgen(js_name = setDevicePixelRatio)]
+pub fn set_device_pixel_ratio(device_pixel_ratio: f32) {
+    DEVICE_PIXEL_RATIO.store(device_pixel_ratio, Ordering::Release);
+    for wav_cache in WAV_CACHES.write().unwrap().values_mut() {
+        wav_cache.update_cache();
+    }
+}
+
+#[wasm_bindgen(js_name = setWav)]
+pub fn set_wav(id_ch_str: &str, wav: WasmFloat32Array, sr: u32, is_clipped: bool) {
+    let wav: Vec<_> = wav.into();
+    if let Some(wav_cache) = WAV_CACHES.read().unwrap().get(id_ch_str)
+        && wav_cache.wav.len() == wav.len()
+        && wav_cache.sr == sr
+        && wav_cache.is_clipped == is_clipped
+        && wav_cache.wav.iter().zip(wav.iter()).all(|(a, b)| a == b)
+    {
+        return; // TODO: remove duplicated calls at the first place
+    }
+    let wav_cache = WavCache::new(wav, sr, is_clipped);
+    WAV_CACHES
+        .write()
+        .unwrap()
+        .insert(id_ch_str.into(), wav_cache);
+}
+
+#[wasm_bindgen(js_name = drawWav)]
+pub fn draw_wav(
+    canvas: &HtmlCanvasElement,
+    ctx: &CanvasRenderingContext2d,
+    id_ch_str: &str,
+    css_width: u32,
+    css_height: u32,
+    start_sec: f32,
+    px_per_sec: f32,
+    amp_range_min: f32,
+    amp_range_max: f32,
+) -> Result<(), JsValue> {
+    let dpr = DEVICE_PIXEL_RATIO.load(Ordering::Acquire);
+    canvas.set_width((css_width as f64 * dpr as f64).round() as u32);
+    canvas.set_height((css_height as f64 * dpr as f64).round() as u32);
+
+    ctx.scale(1. / WAV_IMG_SCALE as f64, 1. / WAV_IMG_SCALE as f64)?;
+
+    let width = canvas.width() as f32 * WAV_IMG_SCALE;
+    let height = canvas.height() as f32 * WAV_IMG_SCALE;
+
+    let is_clipped = {
+        let wav_caches = WAV_CACHES.read().unwrap();
+        match wav_caches.get(id_ch_str) {
+            Some(wav_cache) => wav_cache.is_clipped,
+            None => {
+                return Ok(());
+            }
+        }
+    };
+
+    if is_clipped {
+        let options = WavDrawingOptions {
+            start_sec,
+            px_per_sec,
+            amp_range: (amp_range_min, amp_range_max),
+            ..Default::default()
+        };
+        draw_wav_internal(ctx, id_ch_str, width, height, &options, WAV_CLIPPING_COLOR)?;
+    } else {
+        ctx.clear_rect(0.0, 0.0, width as f64, height as f64);
+    }
+
+    let options = WavDrawingOptions {
+        start_sec,
+        px_per_sec,
+        amp_range: (amp_range_min, amp_range_max),
+        clip_values: if is_clipped { Some((-1., 1.)) } else { None },
+        need_border_for_envelope: !is_clipped,
+        ..Default::default()
+    };
+    draw_wav_internal(ctx, id_ch_str, width, height, &options, WAV_COLOR)?;
+
+    Ok(())
+}
+
+#[wasm_bindgen(js_name = clearWav)]
+pub fn clear_wav(
+    canvas: Option<HtmlCanvasElement>,
+    ctx: Option<CanvasRenderingContext2d>,
+    css_width: u32,
+    css_height: u32,
+) {
+    let dpr = DEVICE_PIXEL_RATIO.load(Ordering::Acquire);
+    let width = css_width as f32 * dpr;
+    let height = css_height as f32 * dpr;
+    if let Some(canvas) = canvas {
+        canvas.set_width(width.round() as u32);
+        canvas.set_height(height.round() as u32);
+    }
+    if let Some(ctx) = ctx {
+        ctx.clear_rect(
+            0.,
+            0.,
+            (width * WAV_IMG_SCALE) as f64,
+            (height * WAV_IMG_SCALE) as f64,
+        );
+    }
+}
+
+pub(crate) fn draw_wav_internal(
+    ctx: &CanvasRenderingContext2d,
+    id_ch_str: &str,
+    width: f32,
+    height: f32,
+    options: &WavDrawingOptions,
+    color: &str,
+) -> Result<(), JsValue> {
+    let stroke_width = options.stroke_width();
+
+    let (line_points, envelopes) = {
+        let wav_caches = WAV_CACHES.read().unwrap();
+        let wav_cache = wav_caches.get(id_ch_str).unwrap();
+        if options.canvas_px_per_sec() >= CACHE_CANVAS_PX_PER_SEC {
+            let WavCache { wav, sr, .. } = wav_cache;
+            calc_line_envelope_points(wav, *sr, width, height, options)
+        } else {
+            wav_cache.transform_line_envelopes(width, height, options)
+        }
+    };
+
+    let line_path = line_points.try_into_path()?;
+    let envelope_paths = match envelopes {
+        Some(envelopes) => {
+            let mut envelope_paths = Vec::with_capacity(envelopes.len());
+            for envelope in envelopes {
+                let path = envelope.try_into_path(stroke_width)?;
+                envelope_paths.push(path);
+            }
+            envelope_paths
+        }
+        None => Vec::new(),
+    };
+
+    let dpr = DEVICE_PIXEL_RATIO.load(Ordering::Acquire);
+
+    // Draw borders for line
+    if options.need_border_for_line {
+        ctx.set_line_cap("round");
+        ctx.set_line_join("round");
+        ctx.set_stroke_style_str(WAV_BORDER_COLOR);
+        ctx.set_line_width((stroke_width + 2.0 * WAV_BORDER_WIDTH * dpr) as f64);
+        ctx.stroke_with_path(&line_path);
+    }
+
+    // Draw borders for envelopes
+    if options.need_border_for_envelope {
+        for path in &envelope_paths {
+            ctx.set_line_cap("round");
+            ctx.set_line_join("round");
+            ctx.set_stroke_style_str(WAV_BORDER_COLOR);
+            ctx.set_line_width((2.0 * WAV_BORDER_WIDTH * dpr) as f64);
+            ctx.stroke_with_path(path);
+        }
+    }
+
+    // Draw main line
+    ctx.set_line_cap("round");
+    ctx.set_line_join("round");
+    ctx.set_stroke_style_str(color);
+    ctx.set_line_width(stroke_width as f64);
+    ctx.stroke_with_path(&line_path);
+
+    // Fill envelopes
+    for path in &envelope_paths {
+        ctx.set_fill_style_str(color);
+        ctx.fill_with_path_2d(path);
+    }
+
+    Ok(())
+}
 
 pub(crate) struct WavDrawingOptions {
     pub(crate) start_sec: f32,
@@ -72,14 +251,96 @@ impl WavDrawingOptions {
     }
 }
 
-struct TransformParams {
-    x_scale: f32,
-    x_offset: f32,
-    y2v_scale: f32,
-    y2v_offset: f32,
-    v_clip_values: (f32, f32),
-    v2y_scale: f32,
-    v2y_offset: f32,
+pub(crate) struct WavCache {
+    wav: Vec<f32>,
+    sr: u32,
+    pub(crate) is_clipped: bool,
+    pub(crate) cache_amp_range: (f32, f32),
+    line_points_cache: WavLinePoints,
+    envelopes_cache: Vec<WavEnvelope>,
+}
+
+impl WavCache {
+    fn new(wav: Vec<f32>, sr: u32, is_clipped: bool) -> Self {
+        let cache_amp_range = {
+            let (min, max) = find_min_max(&wav);
+            (min.min(-1.), max.max(1.))
+        };
+        let mut wav_cache = Self {
+            wav,
+            sr,
+            is_clipped,
+            cache_amp_range,
+            line_points_cache: WavLinePoints::new(),
+            envelopes_cache: Vec::new(),
+        };
+        wav_cache.update_cache();
+        wav_cache
+    }
+
+    fn update_cache(&mut self) {
+        let options = WavDrawingOptions::new_for_cache(self.cache_amp_range);
+        let px_per_samples = (options.canvas_px_per_sec() / self.sr as f32).min(0.1);
+        let width = self.wav.len() as f32 * px_per_samples;
+
+        let (line_points, envelopes) =
+            calc_line_envelope_points(&self.wav, self.sr, width, CACHE_HEIGHT, &options);
+        self.line_points_cache = line_points;
+        self.envelopes_cache = envelopes.unwrap();
+    }
+
+    fn transform_line_envelopes(
+        &self,
+        width: f32,
+        height: f32,
+        options: &WavDrawingOptions,
+    ) -> (WavLinePoints, Option<Vec<WavEnvelope>>) {
+        let px_per_sec = options.canvas_px_per_sec();
+        let x_scale = px_per_sec / CACHE_CANVAS_PX_PER_SEC;
+        let x_offset = -options.start_sec * px_per_sec;
+
+        let y2v_scale = -(self.cache_amp_range.1 - self.cache_amp_range.0) / CACHE_HEIGHT;
+        let y2v_offset = self.cache_amp_range.1;
+        let v_clip_values = options
+            .clip_values
+            .unwrap_or((-f32::INFINITY, f32::INFINITY));
+        let v2y_scale = -height / (options.amp_range.1 - options.amp_range.0).max(1e-8);
+        let v2y_offset = options.offset_y - options.amp_range.1 * v2y_scale;
+
+        let transform_params = &TransformParams {
+            x_scale,
+            x_offset,
+            y2v_scale,
+            y2v_offset,
+            v_clip_values,
+            v2y_scale,
+            v2y_offset,
+        };
+
+        let start_x = (-WAV_MARGIN_PX - x_offset) / x_scale;
+        let end_x = (width + WAV_MARGIN_PX - x_offset) / x_scale;
+
+        let line_capacity =
+            ((width / px_per_sec - options.start_sec) * CACHE_CANVAS_PX_PER_SEC).ceil() as usize;
+        let xformed_line_points = self.line_points_cache.slice_transform(
+            start_x,
+            end_x,
+            &transform_params,
+            Some(line_capacity),
+        );
+
+        let mut xformed_envelopes = Vec::new();
+        for envelope in self.envelopes_cache.iter() {
+            if envelope.xs[0] >= end_x || envelope.xs[envelope.len() - 1] < start_x {
+                continue;
+            }
+            let xformed_envelope =
+                envelope.slice_transform(start_x, end_x, &transform_params, Some(envelope.len()));
+            xformed_envelopes.push(xformed_envelope);
+        }
+
+        (xformed_line_points, Some(xformed_envelopes))
+    }
 }
 
 pub(crate) struct WavLinePoints {
@@ -305,234 +566,14 @@ impl WavEnvelope {
     }
 }
 
-pub(crate) struct WavCache {
-    wav: Vec<f32>,
-    sr: u32,
-    pub(crate) is_clipped: bool,
-    pub(crate) cache_amp_range: (f32, f32),
-    line_points_cache: WavLinePoints,
-    envelopes_cache: Vec<WavEnvelope>,
-}
-
-impl WavCache {
-    fn new(wav: Vec<f32>, sr: u32, is_clipped: bool) -> Self {
-        let cache_amp_range = {
-            let (min, max) = find_min_max(&wav);
-            (min.min(-1.), max.max(1.))
-        };
-        let mut wav_cache = Self {
-            wav,
-            sr,
-            is_clipped,
-            cache_amp_range,
-            line_points_cache: WavLinePoints::new(),
-            envelopes_cache: Vec::new(),
-        };
-        wav_cache.update_cache();
-        wav_cache
-    }
-
-    fn update_cache(&mut self) {
-        let options = WavDrawingOptions::new_for_cache(self.cache_amp_range);
-        let px_per_samples = (options.canvas_px_per_sec() / self.sr as f32).min(0.1);
-        let width = self.wav.len() as f32 * px_per_samples;
-
-        let (line_points, envelopes) =
-            calc_line_envelope_points(&self.wav, self.sr, width, CACHE_HEIGHT, &options);
-        self.line_points_cache = line_points;
-        self.envelopes_cache = envelopes.unwrap();
-    }
-}
-
-#[wasm_bindgen(js_name = setDevicePixelRatio)]
-pub fn set_device_pixel_ratio(device_pixel_ratio: f32) {
-    DEVICE_PIXEL_RATIO.store(device_pixel_ratio, Ordering::Release);
-    for wav_cache in WAV_CACHES.write().unwrap().values_mut() {
-        wav_cache.update_cache();
-    }
-}
-
-#[wasm_bindgen(js_name = setWav)]
-pub fn set_wav(id_ch_str: &str, wav: WasmFloat32Array, sr: u32, is_clipped: bool) {
-    let wav: Vec<_> = wav.into();
-    if let Some(wav_cache) = WAV_CACHES.read().unwrap().get(id_ch_str)
-        && wav_cache.wav.len() == wav.len()
-        && wav_cache.sr == sr
-        && wav_cache.is_clipped == is_clipped
-        && wav_cache.wav.iter().zip(wav.iter()).all(|(a, b)| a == b)
-    {
-        return; // TODO: remove duplicated calls at the first place
-    }
-    let wav_cache = WavCache::new(wav, sr, is_clipped);
-    WAV_CACHES
-        .write()
-        .unwrap()
-        .insert(id_ch_str.into(), wav_cache);
-}
-
-pub(crate) fn draw_wav_internal(
-    ctx: &CanvasRenderingContext2d,
-    id_ch_str: &str,
-    width: f32,
-    height: f32,
-    options: &WavDrawingOptions,
-    color: &str,
-) -> Result<(), JsValue> {
-    let stroke_width = options.stroke_width();
-
-    let (line_points, envelopes) = {
-        let wav_caches = WAV_CACHES.read().unwrap();
-        let WavCache {
-            wav,
-            sr,
-            cache_amp_range: amp_range,
-            line_points_cache,
-            envelopes_cache,
-            ..
-        } = wav_caches.get(id_ch_str).unwrap();
-        if options.canvas_px_per_sec() >= CACHE_CANVAS_PX_PER_SEC {
-            calc_line_envelope_points(wav, *sr, width, height, options)
-        } else {
-            transform_line_envelopes(
-                line_points_cache,
-                envelopes_cache,
-                *amp_range,
-                width,
-                height,
-                options,
-            )
-        }
-    };
-
-    let line_path = line_points.try_into_path()?;
-    let envelope_paths = match envelopes {
-        Some(envelopes) => {
-            let mut envelope_paths = Vec::with_capacity(envelopes.len());
-            for envelope in envelopes {
-                let path = envelope.try_into_path(stroke_width)?;
-                envelope_paths.push(path);
-            }
-            envelope_paths
-        }
-        None => Vec::new(),
-    };
-
-    let dpr = DEVICE_PIXEL_RATIO.load(Ordering::Acquire);
-
-    // Draw borders for line
-    if options.need_border_for_line {
-        ctx.set_line_cap("round");
-        ctx.set_line_join("round");
-        ctx.set_stroke_style_str(WAV_BORDER_COLOR);
-        ctx.set_line_width((stroke_width + 2.0 * WAV_BORDER_WIDTH * dpr) as f64);
-        ctx.stroke_with_path(&line_path);
-    }
-
-    // Draw borders for envelopes
-    if options.need_border_for_envelope {
-        for path in &envelope_paths {
-            ctx.set_line_cap("round");
-            ctx.set_line_join("round");
-            ctx.set_stroke_style_str(WAV_BORDER_COLOR);
-            ctx.set_line_width((2.0 * WAV_BORDER_WIDTH * dpr) as f64);
-            ctx.stroke_with_path(path);
-        }
-    }
-
-    // Draw main line
-    ctx.set_line_cap("round");
-    ctx.set_line_join("round");
-    ctx.set_stroke_style_str(color);
-    ctx.set_line_width(stroke_width as f64);
-    ctx.stroke_with_path(&line_path);
-
-    // Fill envelopes
-    for path in &envelope_paths {
-        ctx.set_fill_style_str(color);
-        ctx.fill_with_path_2d(path);
-    }
-
-    Ok(())
-}
-
-#[wasm_bindgen(js_name = drawWav)]
-pub fn draw_wav(
-    canvas: &HtmlCanvasElement,
-    ctx: &CanvasRenderingContext2d,
-    id_ch_str: &str,
-    css_width: u32,
-    css_height: u32,
-    start_sec: f32,
-    px_per_sec: f32,
-    amp_range_min: f32,
-    amp_range_max: f32,
-) -> Result<(), JsValue> {
-    let dpr = DEVICE_PIXEL_RATIO.load(Ordering::Acquire);
-    canvas.set_width((css_width as f64 * dpr as f64).round() as u32);
-    canvas.set_height((css_height as f64 * dpr as f64).round() as u32);
-
-    ctx.scale(1. / WAV_IMG_SCALE as f64, 1. / WAV_IMG_SCALE as f64)?;
-
-    let width = canvas.width() as f32 * WAV_IMG_SCALE;
-    let height = canvas.height() as f32 * WAV_IMG_SCALE;
-
-    let is_clipped = {
-        let wav_caches = WAV_CACHES.read().unwrap();
-        match wav_caches.get(id_ch_str) {
-            Some(wav_cache) => wav_cache.is_clipped,
-            None => {
-                return Ok(());
-            }
-        }
-    };
-
-    if is_clipped {
-        let options = WavDrawingOptions {
-            start_sec,
-            px_per_sec,
-            amp_range: (amp_range_min, amp_range_max),
-            ..Default::default()
-        };
-        draw_wav_internal(ctx, id_ch_str, width, height, &options, WAV_CLIPPING_COLOR)?;
-    } else {
-        ctx.clear_rect(0.0, 0.0, width as f64, height as f64);
-    }
-
-    let options = WavDrawingOptions {
-        start_sec,
-        px_per_sec,
-        amp_range: (amp_range_min, amp_range_max),
-        clip_values: if is_clipped { Some((-1., 1.)) } else { None },
-        need_border_for_envelope: !is_clipped,
-        ..Default::default()
-    };
-    draw_wav_internal(ctx, id_ch_str, width, height, &options, WAV_COLOR)?;
-
-    Ok(())
-}
-
-#[wasm_bindgen(js_name = clearWav)]
-pub fn clear_wav(
-    canvas: Option<HtmlCanvasElement>,
-    ctx: Option<CanvasRenderingContext2d>,
-    css_width: u32,
-    css_height: u32,
-) {
-    let dpr = DEVICE_PIXEL_RATIO.load(Ordering::Acquire);
-    let width = css_width as f32 * dpr;
-    let height = css_height as f32 * dpr;
-    if let Some(canvas) = canvas {
-        canvas.set_width(width.round() as u32);
-        canvas.set_height(height.round() as u32);
-    }
-    if let Some(ctx) = ctx {
-        ctx.clear_rect(
-            0.,
-            0.,
-            (width * WAV_IMG_SCALE) as f64,
-            (height * WAV_IMG_SCALE) as f64,
-        );
-    }
+struct TransformParams {
+    x_scale: f32,
+    x_offset: f32,
+    y2v_scale: f32,
+    y2v_offset: f32,
+    v_clip_values: (f32, f32),
+    v2y_scale: f32,
+    v2y_offset: f32,
 }
 
 fn calc_line_envelope_points(
@@ -663,55 +704,4 @@ fn calc_line_envelope_points(
         line_points.push(last_x_mid, last_y);
     }
     (line_points, Some(envelopes))
-}
-
-fn transform_line_envelopes(
-    line_points: &WavLinePoints,
-    envelopes: &[WavEnvelope],
-    amp_range: (f32, f32),
-    width: f32,
-    height: f32,
-    options: &WavDrawingOptions,
-) -> (WavLinePoints, Option<Vec<WavEnvelope>>) {
-    let px_per_sec = options.canvas_px_per_sec();
-    let x_scale = px_per_sec / CACHE_CANVAS_PX_PER_SEC;
-    let x_offset = -options.start_sec * px_per_sec;
-
-    let y2v_scale = -(amp_range.1 - amp_range.0) / CACHE_HEIGHT;
-    let y2v_offset = amp_range.1;
-    let v_clip_values = options
-        .clip_values
-        .unwrap_or((-f32::INFINITY, f32::INFINITY));
-    let v2y_scale = -height / (options.amp_range.1 - options.amp_range.0).max(1e-8);
-    let v2y_offset = options.offset_y - options.amp_range.1 * v2y_scale;
-
-    let transform_params = &TransformParams {
-        x_scale,
-        x_offset,
-        y2v_scale,
-        y2v_offset,
-        v_clip_values,
-        v2y_scale,
-        v2y_offset,
-    };
-
-    let start_x = (-WAV_MARGIN_PX - x_offset) / x_scale;
-    let end_x = (width + WAV_MARGIN_PX - x_offset) / x_scale;
-
-    let line_capacity =
-        ((width / px_per_sec - options.start_sec) * CACHE_CANVAS_PX_PER_SEC).ceil() as usize;
-    let xformed_line_points =
-        line_points.slice_transform(start_x, end_x, &transform_params, Some(line_capacity));
-
-    let mut xformed_envelopes = Vec::new();
-    for envelope in envelopes {
-        if envelope.xs[0] >= end_x || envelope.xs[envelope.len() - 1] < start_x {
-            continue;
-        }
-        let xformed_envelope =
-            envelope.slice_transform(start_x, end_x, &transform_params, Some(envelope.len()));
-        xformed_envelopes.push(xformed_envelope);
-    }
-
-    (xformed_line_points, Some(xformed_envelopes))
 }
