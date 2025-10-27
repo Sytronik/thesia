e8da565a77d59a901737e8fa6b73d567f813234e
Sytronik <sytronik210@gmail.com>
2025-10-27 15:54:01 +0900

    unuse unnecessary features of web-sys, pin versions of wasm-renderer's dependency

--- thesia-wasm-renderer/src/lib.rs
+++ thesia-wasm-renderer/src/lib.rs
@@ -1,22 +1,4 @@
-use wasm_bindgen::prelude::*;
-
 mod mem;
 mod overview;
 mod simd;
 mod wav;
-
-// Import the `console.log` function from the browser's console
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    pub(crate) fn log(s: &str);
-}
-
-// Define a macro to provide `println!(..)`-style syntax for `console.log` logging.
-#[allow(unused_macros)]
-#[macro_export]
-macro_rules! console_log {
-    ( $( $t:tt )* ) => {
-        crate::log(&format!( $( $t )* ))
-    }
-}
