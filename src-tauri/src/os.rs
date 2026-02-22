b9b99c2020267076ccffab94f6b944b04cd1e16e
Sytronik <sytronik210@gmail.com>
2026-02-22 15:33:20 +0900

    os.rs considers ios

--- src-tauri/src/os.rs
+++ src-tauri/src/os.rs
@@ -1,6 +1,6 @@
 #[inline(always)]
 pub const fn os_label<'a>(macos: &'a str, other: &'a str) -> &'a str {
-    if cfg!(target_os = "macos") {
+    if cfg!(any(target_os = "macos", target_os = "ios")) {
         macos
     } else {
         other
