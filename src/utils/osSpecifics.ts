ff3d159db98d16898a03af02be1a5de017a58fe9
Sytronik <sytronik210@gmail.com>
2026-06-03 18:44:39 +0900

    npm update

--- src/utils/osSpecifics.ts
+++ src/utils/osSpecifics.ts
@@ -1,4 +1,3 @@
-import { Hotkey } from "react-hotkeys-hook/dist/types";
 import { platform } from "@tauri-apps/plugin-os";
 
 export function isApple() {
@@ -26,13 +25,4 @@ export function isCommandOnly(event: MouseOrKeyboardEvent) {
   }
   // Windows, Linux, UNIX
   return event.ctrlKey && !event.metaKey && !event.shiftKey && !event.altKey;
-}
-
-export function isHotkeyCommandOnly(hotkey: Hotkey) {
-  // Returns true if Ctrl or cmd keys were pressed.
-  if (isApple()) {
-    return hotkey.meta && !hotkey.ctrl && !hotkey.shift && !hotkey.alt;
-  }
-  // Windows, Linux, UNIX
-  return hotkey.ctrl && !hotkey.meta && !hotkey.shift && !hotkey.alt;
 }
