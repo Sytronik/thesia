ca84bcee6a0b8c7d43c845b34c99872a6b0cd23a
Sytronik <sytronik210@gmail.com>
2026-07-12 12:38:35 +0900

    upgrade react tooltip to 6.0.8

--- src/prototypes/MainViewer/TrackSummary.tsx
+++ src/prototypes/MainViewer/TrackSummary.tsx
@@ -94,10 +94,9 @@ function TrackSummary(props: TrackSummaryProps) {
     const guardClipStatsLines = data.guardClipStats.map(([channel, stat]) =>
       channel >= 0 ? `${CHANNEL[chCount][channel]}: ${stat}` : stat,
     );
-    const formattedGuardClipStats = guardClipStatsLines.join("<br />");
     return (
       <>
-        <span data-tooltip-id="guardclipstat-tooltip" data-tooltip-html={formattedGuardClipStats}>
+        <span data-tooltip-id="guardclipstat-tooltip">
           <svg
             fill="#FFFFFF"
             xmlns="http://www.w3.org/2000/svg"
@@ -108,7 +107,14 @@ function TrackSummary(props: TrackSummaryProps) {
             <path d="M 64 6 C 32 6 6 32 6 64 C 6 96 32 122 64 122 C 96 122 122 96 122 64 C 122 32 96 6 64 6 z M 64 12 C 92.7 12 116 35.3 116 64 C 116 92.7 92.7 116 64 116 C 35.3 116 12 92.7 12 64 C 12 35.3 35.3 12 64 12 z M 64 30 A 9 9 0 0 0 64 48 A 9 9 0 0 0 64 30 z M 64 59 C 59 59 55 63 55 68 L 55 92 C 55 97 59 101 64 101 C 69 101 73 97 73 92 L 73 68 C 73 63 69 59 64 59 z" />
           </svg>
         </span>
-        <Tooltip id="guardclipstat-tooltip" place="bottom" positionStrategy="fixed" />
+        <Tooltip id="guardclipstat-tooltip" place="bottom" positionStrategy="fixed">
+          {guardClipStatsLines.map((line, index) => (
+            <React.Fragment key={`${index}-${line}`}>
+              {index > 0 ? <br /> : null}
+              {line}
+            </React.Fragment>
+          ))}
+        </Tooltip>
       </>
     );
   };
